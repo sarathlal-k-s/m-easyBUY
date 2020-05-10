1564c1b2032aeb1922ec445102875e4c52d448a5
sarathlal-k-s <sarathlalks240@gmail.com>
2020-05-11 00:07:33 +0530

    welcome,login,register screen

--- app/src/main/java/com/sarath/m_easybuy/RegisterActivity.java
+++ app/src/main/java/com/sarath/m_easybuy/RegisterActivity.java
@@ -1,15 +1,30 @@
 package com.sarath.m_easybuy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private Button regButton;
     private TextView loginText;
+    private EditText editTextUsername,editTextEmail,editTextPassword;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +38,21 @@ public class RegisterActivity extends AppCompatActivity {
                 openLogin();
             }
         });
-    }
 
+        regButton = (Button) findViewById(R.id.RegisterButton);
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextUsername = (EditText) findViewById(R.id.usernameRegister);
+        editTextEmail = (EditText) findViewById(R.id.emailRegister);
+        editTextPassword = (EditText) findViewById(R.id.passwordRegister);
+    }
 
     public void openLogin(){
         Intent intent = new Intent(this,LoginActivity.class);
@@ -35,6 +63,59 @@ public class RegisterActivity extends AppCompatActivity {
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
     }
+
+    public void registerUser(){
+        String username = editTextUsername.getText().toString().trim();
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+
+        if(username.isEmpty()){
+            editTextUsername.setError("Username Required");
+            editTextUsername.requestFocus();
+            return;
+        }
+
+
+        if(email.isEmpty()){
+            editTextEmail.setError("Email-id Required");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Enter a valid email-id");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            editTextPassword.setError("Password Required");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if(password.length()<6){
+            editTextPassword.setError("Minimum length of password is 6");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(),"User Registration Successfull",Toast.LENGTH_SHORT).show();
+                    openLogin();
+                    Toast.makeText(getApplicationContext(),"Login with your account",Toast.LENGTH_LONG).show();
+                }else{
+                    if(task.getException() instanceof FirebaseAuthUserCollisionException){
+                        Toast.makeText(getApplicationContext(),"This email-id is already registered",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+    }
+
 
     @Override
     public void onBackPressed(){
