e01b98de07eb1e5a76951b956d7f13482e7e1663
sarathlal-k-s <sarathlalks240@gmail.com>
2020-05-12 00:55:54 +0530

    welcome,login,register screen

--- app/src/main/java/com/sarath/m_easybuy/FeedActivity.java
+++ app/src/main/java/com/sarath/m_easybuy/FeedActivity.java
@@ -1,8 +1,14 @@
 package com.sarath.m_easybuy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class FeedActivity extends AppCompatActivity {
 
@@ -10,5 +16,33 @@ public class FeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new search()).commit();
+        }
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment selectedFragment = null;
+
+                switch (menuItem.getItemId()){
+                    case R.id.search :
+                        selectedFragment = new search();
+                        break;
+                    case R.id.post :
+                        selectedFragment = new post();
+                        break;
+                    case R.id.profile :
+                        selectedFragment = new profile();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,selectedFragment).commit();
+                return true;
+            }
+        });
+
     }
+
 }
---
+++ app/src/main/java/com/sarath/m_easybuy/post.java
@@ -0,0 +1,28 @@
+package com.sarath.m_easybuy;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class post extends Fragment {
+
+    public post() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_post, container, false);
+    }
+}
