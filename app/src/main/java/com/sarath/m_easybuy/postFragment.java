663c7c0f29b254d0fb656a69e76353a261b50b9f
sarathlal-k-s <sarathlalks240@gmail.com>
2020-05-20 20:38:10 +0530

    search,post,profile wip always logged in select image

--- app/src/main/java/com/sarath/m_easybuy/postFragment.java
+++ app/src/main/java/com/sarath/m_easybuy/postFragment.java
@@ -1,5 +1,8 @@
 package com.sarath.m_easybuy;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 /**
@@ -18,11 +22,33 @@ public class postFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private ImageView imageViewUpload;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post, container, false);
+        View view = inflater.inflate(R.layout.fragment_post, container, false);
+        imageViewUpload = view.findViewById(R.id.imageViewUpload);
+        imageViewUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent,1);
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1 && resultCode == Activity.RESULT_OK){
+            Uri selectedImageUri = data.getData();
+            imageViewUpload.setImageURI(selectedImageUri);
+        }
     }
 }
