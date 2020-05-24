cf6a32e543cc2fe4d6c87566c78e3d82d554ac6e
sarathlal-k-s <sarathlalks240@gmail.com>
2020-05-24 18:32:13 +0530

    ads can be viewed

---
+++ app/src/main/java/com/sarath/m_easybuy/AdPageActivity.java
@@ -0,0 +1,18 @@
+package com.sarath.m_easybuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adpage);
+
+
+
+
+    }
+}
