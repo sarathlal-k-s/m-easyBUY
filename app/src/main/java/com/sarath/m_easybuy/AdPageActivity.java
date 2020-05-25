b1784345af0472510e0464fbf82dbe0ffe15d68e
sarathlal-k-s <sarathlalks240@gmail.com>
2020-05-25 16:10:12 +0530

    Ad page layout fixed

--- app/src/main/java/com/sarath/m_easybuy/AdPageActivity.java
+++ app/src/main/java/com/sarath/m_easybuy/AdPageActivity.java
@@ -3,8 +3,10 @@ package com.sarath.m_easybuy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -40,5 +42,19 @@ public class AdPageActivity extends AppCompatActivity {
             price.setText("₹ "+adItem.getPrice());
             phone.setText(adItem.getPhone());
         }
+
+        final String phNo = phone.getText().toString().trim();
+
+        Button callButton = findViewById(R.id.ButtonContactSeller);
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:"+phNo));
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
